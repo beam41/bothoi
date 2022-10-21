497de8a63418b0ce9b977c940cb2e8bdda707ea8
Meehoi <beam_baimon@hotmail.com>
2022-10-21 15:48:27 +0700

    add some unit test

--- util/generics.go
+++ util/generics.go
@@ -1,5 +1,16 @@
 package util
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+func Min[T constraints.Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 func Ternary[T any](condition bool, trueVal, falseVal T) T {
 	if condition {
 		return trueVal
---
+++ util/generics_test.go
@@ -0,0 +1,60 @@
+package util
+
+import (
+	"testing"
+)
+
+func Test_Min_ReturnSmallerValueInt(t *testing.T) {
+	result := Min(3, 1)
+	expected := 1
+
+	if result != expected {
+		t.Errorf("expected: %v, got: %v", expected, result)
+	} else {
+		t.Logf("expected: %v, got: %v", expected, result)
+	}
+}
+
+func Test_Min_ReturnSmallerValueFloat(t *testing.T) {
+	result := Min(3.0, 1.0)
+	expected := 1.0
+
+	if result != expected {
+		t.Errorf("expected: %v, got: %v", expected, result)
+	} else {
+		t.Logf("expected: %v, got: %v", expected, result)
+	}
+}
+
+func Test_Min_ReturnSmallerValueStr(t *testing.T) {
+	result := Min("apple", "bee")
+	expected := "apple"
+
+	if result != expected {
+		t.Errorf("expected: %v, got: %v", expected, result)
+	} else {
+		t.Logf("expected: %v, got: %v", expected, result)
+	}
+}
+
+func Test_Ternary_ReturnFirstArgumentWhenTrue(t *testing.T) {
+	result := Ternary(true, 1, 2)
+	expected := 1
+
+	if result != expected {
+		t.Errorf("expected: %v, got: %v", expected, result)
+	} else {
+		t.Logf("expected: %v, got: %v", expected, result)
+	}
+}
+
+func Test_Ternary_ReturnSecondArgumentWhenFalse(t *testing.T) {
+	result := Ternary(false, 1, 2)
+	expected := 2
+
+	if result != expected {
+		t.Errorf("expected: %v, got: %v", expected, result)
+	} else {
+		t.Logf("expected: %v, got: %v", expected, result)
+	}
+}
